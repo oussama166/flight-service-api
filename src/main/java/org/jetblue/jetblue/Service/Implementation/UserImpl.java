ec1ff96103f7dc84608a7fb27988df5cf83e08ca
oussama166 <oussamaouardi80@gmail.com>
2025-02-27 15:48:36 +0000

    Updating the userImpl

--- src/main/java/org/jetblue/jetblue/Service/Implementation/UserImpl.java
+++ src/main/java/org/jetblue/jetblue/Service/Implementation/UserImpl.java
@@ -1,18 +1,14 @@
 package org.jetblue.jetblue.Service.Implementation;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.jetblue.jetblue.Mapper.User.UserMapper;
 import org.jetblue.jetblue.Mapper.User.UserResponseBasic;
 import org.jetblue.jetblue.Models.DAO.User;
-import org.jetblue.jetblue.Models.DTO.UserBasicDTO;
 import org.jetblue.jetblue.Repositories.UserRepo;
 import org.jetblue.jetblue.Service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
